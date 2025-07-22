59040145c669eef78a4ba6168944fdd18663770f
jerem-greencat <82501643+jerem-greencat@users.noreply.github.com>
2025-07-22 17:19:58 +0200

    rename credits

--- src/infrastructure/discord/events/interactionCreate.js
+++ src/infrastructure/discord/events/interactionCreate.js
@@ -164,7 +164,7 @@ export default async function onInteractionCreate(interaction) {
       );
       
       return interaction.reply({
-        content: `✅ ${amount} crédits retirés du compte de <@${userId}>.`,
+        content: `✅ ${amount} euros retirés du compte de <@${userId}>.`,
         ephemeral: true
       });
     }
